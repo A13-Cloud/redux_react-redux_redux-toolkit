98091aa8616f904908a3a01785fd5ed7637a24dd
Avik013 <gukasyan.spa@gmail.com>
2023-01-19 23:51:08 +0400

    add reducer of list item creator

--- redux-todo/src/app/redux/reducers/root/rootReducer.js
+++ redux-todo/src/app/redux/reducers/root/rootReducer.js
@@ -1,6 +1,7 @@
 import {combineReducers} from "redux";
+import listItemCreatorReducer from "../listItemCreatorReducer";
 
 
 export const rootReducer = combineReducers({
-
+    listItemCreatorReducer
 });
