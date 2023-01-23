1a30505ae3cd8ab24ff743c71619789011fa2e37
Avik013 <gukasyan.spa@gmail.com>
2023-01-23 23:57:17 +0400

    feat(redux): add list item update action for list item

---
+++ redux-todo/src/app/redux/actions/listItemUpdateAction.js
@@ -0,0 +1,9 @@
+import {LIST_ITEM_UPDATE} from "../types/types";
+
+export const listItemUpdateAction = (text, id) =>
+{
+    // return {
+    //     type: LIST_ITEM_UPDATE,
+    //     data: {text, id}
+    // }
+};
